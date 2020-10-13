52a122a094476b8ae24a8c78502e769c5e9de045
fthorde <manyids2@gmail.com>
2020-10-13 12:34:17 +0200

    Added definition and tests for MKDGradients.

---
+++ kornia/feature/mkd.py
@@ -0,0 +1,86 @@
+from typing import Union, Tuple, Dict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from kornia.utils import create_meshgrid
+from kornia.geometry.conversions import cart2pol
+from kornia.filters import SpatialGradient
+
+
+sqrt2 = torch.tensor(1.4142135623730951)  # pylint: disable=E1102
+
+# Precomputed coefficients for Von Mises kernel, given N and K(appa).
+COEFFS_N1_K1 = [0.38214156, 0.48090413]
+COEFFS_N2_K8 = [0.14343168, 0.268285, 0.21979234]
+COEFFS_N3_K8 = [0.14343168, 0.268285, 0.21979234, 0.15838885]
+COEFFS = {'xy':COEFFS_N1_K1, 'rhophi':COEFFS_N2_K8, 'theta':COEFFS_N3_K8}
+
+
+urls: Dict[str, str] = {k:f'https://github.com/manyids2/mkd_pytorch/raw/master/mkd_pytorch/mkd-{k}-64.pth'
+                        for k in ['cart', 'polar', 'concat']}
+
+
+def get_grid_dict(patch_size:int = 32) -> Dict[str, torch.Tensor]:
+    """Gets cartesian and polar parametrizations of grid. """
+    kgrid = create_meshgrid(height=patch_size,
+                            width=patch_size,
+                            normalized_coordinates=True)
+    x = kgrid[0,:,:,0]
+    y = kgrid[0,:,:,1]
+    rho, phi = cart2pol(x, y)
+    grid_dict = {'x':x, 'y':y, 'rho':rho, 'phi':phi}
+    return grid_dict
+
+
+def get_kron_order(d1: int, d2: int) -> torch.Tensor:
+    """Gets order for doing kronecker product. """
+    kron_order = torch.zeros([d1 * d2, 2], dtype=torch.int64)
+    for i in range(d1):
+        for j in range(d2):
+            kron_order[i * d2 + j, 0] = i
+            kron_order[i * d2 + j, 1] = j
+    return kron_order
+
+
+class MKDGradients(nn.Module):
+    r"""
+    Module, which computes gradients of given patches,
+    stacked as [magnitudes, orientations].
+    Given gradients $g_x$, $g_y$ with respect to $x$, $y$ respectively,
+      - $\mathbox{mags} = $\sqrt{g_x^2 + g_y^2 + eps}$
+      - $\mathbox{oris} = $\mbox{tan}^{-1}(\nicefrac{g_y}{g_x})$.
+    Args:
+        patch_size: (int) Input patch size in pixels (32 is default)
+    Returns:
+        Tensor: gradients of given patches
+    Shape:
+        - Input: (B, 1, patch_size, patch_size)
+        - Output: (B, 2, patch_size, patch_size)
+    Examples::
+        >>> patches = torch.rand(23, 1, 32, 32)
+        >>> gradient = kornia.feature.mkd.MKDGradients(patch_size=32)
+        >>> g = gradient(patches) # 23x2x32x32
+    """
+
+    def __init__(self, eps: float = 1e-8) -> None:
+        super().__init__()
+        self.eps = eps
+
+        # Modify 'diff' gradient.
+        grad_fn = SpatialGradient(mode='diff', order=1, normalized=False)
+        grad_fn.kernel = -1 * grad_fn.kernel
+        self.grad = grad_fn
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        grads_xy = self.grad(x)[:,0,:,:,:]
+        gx = grads_xy[:,0,:,:].unsqueeze(1)
+        gy = grads_xy[:,1,:,:].unsqueeze(1)
+        mags = torch.sqrt(torch.pow(gx, 2) + torch.pow(gy, 2) + self.eps)
+        oris = torch.atan2(gy, gx)
+        y = torch.cat([mags, oris], dim=1)
+        return y
+
+    def __repr__(self):
+        return self.__class__.__name__ +\
+            '(' + 'eps=' + str(self.eps) + ')'
+
---
+++ test/feature/test_mkd.py
@@ -0,0 +1,63 @@
+import pytest
+import kornia.testing as utils  # test utils
+
+from torch.testing import assert_allclose
+from torch.autograd import gradcheck
+from kornia.feature.mkd import *
+
+
+@pytest.mark.parametrize("ps", [5, 13, 25])
+def test_get_grid_dict(ps):
+    grid_dict = get_grid_dict(ps)
+    param_keys = ['x','y','phi','rho']
+    assert set(grid_dict.keys()) == set(param_keys)
+    for k in param_keys:
+        assert grid_dict[k].shape == (ps, ps)
+
+
+@pytest.mark.parametrize("d1,d2",
+                         [(1, 1), (1,2), (2,1), (5,6)])
+def test_get_kron_order(d1,d2):
+    out = get_kron_order(d1, d2)
+    assert out.shape == (d1 * d2, 2)
+
+
+class TestMKDGradients:
+    @pytest.mark.parametrize("ps", [5, 13, 25])
+    def test_shape(self, ps, device):
+        inp = torch.ones(1, 1, ps, ps, device=device)
+        gradients = MKDGradients().to(device)
+        out = gradients(inp)
+        assert out.shape == (1, 2, ps, ps)
+
+    @pytest.mark.parametrize("bs", [1, 5, 13])
+    def test_batch_shape(self, bs, device):
+        inp = torch.ones(bs, 1, 15, 15, device=device)
+        gradients = MKDGradients().to(device)
+        out = gradients(inp)
+        assert out.shape == (bs, 2, 15, 15)
+
+    def test_print(self, device):
+        gradients = MKDGradients().to(device)
+        gradients.__repr__()
+
+    def test_toy(self, device):
+        patch = torch.ones(1, 1, 6, 6, device=device).float()
+        patch[0, 0, :, 3:] = 0
+        gradients = MKDGradients().to(device)
+        out = gradients(patch)
+        expected = torch.Tensor([0, 0, 1., 1., 0, 0], device=device)
+        expected_mags = expected.unsqueeze(0).repeat(6,1)
+        expected_oris = expected_mags * 0
+        assert_allclose(out[0,0,:,:], expected_mags, atol=1e-3, rtol=1e-3)
+        assert_allclose(out[0,1,:,:], expected_oris, atol=1e-3, rtol=1e-3)
+
+    def test_gradcheck(self, device):
+        batch_size, channels, height, width = 1, 1, 13, 13
+        patches = torch.rand(batch_size, channels, height, width, device=device)
+        patches = utils.tensor_to_gradcheck_var(patches)  # to var
+
+        def grad_describe(patches):
+            return MKDGradients()(patches)
+        assert gradcheck(grad_describe, (patches),
+                         raise_exception=True, nondet_tol=1e-4)
